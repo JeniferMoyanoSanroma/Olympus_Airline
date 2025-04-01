bd50c14228a7d2b1f27932ae95ee25c31498e70b
JeniferMoyano <jenifermoyano2006>
2025-04-01 15:29:32 +0200

    feat: Home structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="container-home">
+    <div class="container-title">
+        <h1>OLYMPUS</h1>
+        <h2>AIRLINE</h2>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="container-button">
+            <p>"Más rápido que Hermes, más alto que Urano."</p>
+            <button>COMENZAR</button>
         </div>
     </div>
 </div>
