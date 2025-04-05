84e9295a81147583f0847686680f4d609c12e058
JeniferMoyano <jenifermoyano2006>
2025-04-06 00:57:52 +0200

    feat: Vista de Home completada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,17 @@
-@extends('layouts.app')
+@extends('layouts.app1')
+@section('title', 'Olympus Airline')
+<link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/512/0/614.png" type="image/x-icon">
 
 @section('content')
+    <div class="container-home">
+        <img src="../img/fondoHome.jpg" class="container-img">
 
-<div class="container-home">
-    <div class="container-title">
-        <h1>OLYMPUS</h1>
-        <h2>AIRLINE</h2>
-
-        <div class="container-button">
-            <p>"Más rápido que Hermes, más alto que Urano."</p>
-            <button>COMENZAR</button>
+        <div class="container-title">
+            <h1>OLYMPUS</h1>
+            
+            <div class="container-button">
+                <p>"Más rápido que Hermes, más alto que Urano."</p>
+                <button>COMENZAR</button>
+            </div>
         </div>
     </div>
-</div>
-@endsection
