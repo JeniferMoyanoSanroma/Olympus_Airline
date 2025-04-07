11f4b119ee4b2b5f6f415947b51ac9bacda688d7
JeniferMoyano <jenifermoyano2006>
2025-04-07 19:07:59 +0200

    feat: Modelo de aviones creados

--- app/Models/Plane.php
+++ app/Models/Plane.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Plane extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'max_seats',
+    ];
 }
