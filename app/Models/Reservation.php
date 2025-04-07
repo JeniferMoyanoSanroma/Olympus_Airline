9ad85832827fce399bad79775677bfa12aa53d59
JeniferMoyano <jenifermoyano2006>
2025-04-07 19:15:18 +0200

    feat: Modelo de reservas creadas

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'flight_id',
+        'status',
+    ];
 }
